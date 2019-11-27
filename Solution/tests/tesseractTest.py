54d2479bcff9dcc9a42675a6777a5775a64c2d26
Abdulrazak Osman <25930486+iamabdulrazak@users.noreply.github.com>
2019-11-27 21:38:18 +0300

    Update tesseractTest.py

--- Solution/tests/tesseractTest.py
+++ Solution/tests/tesseractTest.py
@@ -2,8 +2,8 @@ from PIL import Image
 import pytesseract
 
 def extractWords():
-    img1 = Image.open("F:\Projects\DatathonOpenData - Dataology\Solution\images\img1.jpg")
-    img2 = Image.open("F:\Projects\DatathonOpenData - Dataology\Solution\images\img2.jpg")
+    img1 = Image.open("Solution\images\img1.jpg")
+    img2 = Image.open("Solution\images\img2.jpg")
     engText = pytesseract.image_to_string(img1, lang='eng')
     araText = pytesseract.image_to_string(img2, lang='ara')
     print(engText)
